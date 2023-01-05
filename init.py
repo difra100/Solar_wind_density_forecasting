a484e998e9d2bb30ffb7b2a72626de66f4cb6a28
massimocoppotelli <coppotelli.1705325@studenti.uniroma1.it>
2023-01-05 20:09:40 +0100

    some examples are going on

--- init.py
+++ init.py
@@ -5,3 +5,17 @@ H = 4  # day
 D = 2  # day
 
 resolution = pd.Timedelta(1, 'd')
+
+
+
+
+
+
+#architecture hyperparameters
+n_channels = 1
+n_hidden_channels = 768
+kernel_size = (3,3)
+n_layers = 1
+batch_first = True
+bias = True
+
